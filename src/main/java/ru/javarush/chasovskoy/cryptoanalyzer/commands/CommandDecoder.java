a938030a462c93d5ed6439b5696ef58d7400a2e2
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2024-12-19 20:12:43 +0100

    added basic but fully working decoding

--- src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/commands/CommandDecoder.java
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/commands/CommandDecoder.java
@@ -1,11 +1,50 @@
 package ru.javarush.chasovskoy.cryptoanalyzer.commands;
 
+import ru.javarush.chasovskoy.cryptoanalyzer.constants.Constants;
 import ru.javarush.chasovskoy.cryptoanalyzer.entity.Result;
 import ru.javarush.chasovskoy.cryptoanalyzer.entity.ResultCode;
+import ru.javarush.chasovskoy.cryptoanalyzer.exceptions.AppException;
+
+import java.io.*;
+import java.nio.file.Path;
 
 public class CommandDecoder implements Action {
     @Override
     public Result execute(String[] parameters) {
-        return new Result("decode all right", ResultCode.OK);
+        if (parameters.length < 3) {
+            return new Result("Insufficient parameters. Expected: [inputFile, outputFile, shift]", ResultCode.ERROR);
+        }
+
+        Path textPath = Path.of(Constants.TXT_FOLDER, parameters[0]);
+        Path decodedPath = Path.of(Constants.TXT_FOLDER, parameters[1]);
+        int shift;
+
+        try {
+            shift = Integer.parseInt(parameters[2]);
+            shift *= -1;
+        } catch (NumberFormatException e) {
+            return new Result("Invalid shift value. Must be an integer.", ResultCode.ERROR);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(textPath.toString()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(decodedPath.toString()))) {
+
+            char[] buffer = new char[Constants.CHUNK_SIZE];
+            int bytesRead;
+
+            while ((bytesRead = reader.read(buffer)) != -1) {
+                // Transform the read characters using Caesar cipher
+                char[] decodedBuffer = new char[bytesRead];
+                for (int i = 0; i < bytesRead; i++) {
+                    decodedBuffer[i] = CommandCharShifter.shiftCharacter(buffer[i], shift);
+                }
+                // Write transformed characters to the output file
+                writer.write(decodedBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            throw new AppException("Error accessing file: " + textPath, e);
+        }
+
+        return new Result("Encoding completed successfully. Output written to: " + decodedPath, ResultCode.OK);
     }
 }
--- src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/constants/Constants.java
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/constants/Constants.java
@@ -8,9 +8,13 @@ public class Constants {
     private static final String numbers = "0123456789";
     private static final String punctuation = "~!@#$%^&*()_-+={}[];:'\"/?.>,<\\";
     public static final String ALPHABET = rus + rus.toLowerCase() + eng + eng.toLowerCase() + numbers + punctuation;
+
     public static final int CHUNK_SIZE = 1024;
+
+    // Default saving directory
     public static final String TXT_FOLDER = System.getProperty("user.dir") + File.separator + "texts" + File.separator;
 
+    // Check if the default directory exists or can be created
     static {
         File directory = new File(TXT_FOLDER);
         if (!directory.exists() && !directory.mkdirs()) {
