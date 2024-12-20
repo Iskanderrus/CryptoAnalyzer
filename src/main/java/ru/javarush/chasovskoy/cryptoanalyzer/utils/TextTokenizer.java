711ae4258c0ddc29e91158a489a300deca3a5569
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2024-12-20 21:48:45 +0100

    brute force logic based on word frequency

---
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/utils/TextTokenizer.java
@@ -0,0 +1,5 @@
+package ru.javarush.chasovskoy.cryptoanalyzer.utils;
+
+public class TextTokenizer {
+    // takes file as input and returns tokens of the text
+}
---
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/utils/WordFrequencyAnalyzer.java
@@ -0,0 +1,9 @@
+package ru.javarush.chasovskoy.cryptoanalyzer.utils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class WordFrequencyAnalyzer {
+    // takes tokens as input and returns a hashmap with the most frequent
+    // words if they have length from 3 to 6 characters (top 10 in each category)
+}
