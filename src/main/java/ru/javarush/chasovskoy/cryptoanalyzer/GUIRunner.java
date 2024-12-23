37b031056d502096b5a154be6750a8cf37c45271
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2024-12-24 00:33:36 +0100

    initial frame layout draft 1

--- src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/GUIRunner.java
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/GUIRunner.java
@@ -1,21 +1,62 @@
 package ru.javarush.chasovskoy.cryptoanalyzer;
 
-import ru.javarush.chasovskoy.cryptoanalyzer.entity.Result;
+import ru.javarush.chasovskoy.cryptoanalyzer.gui.MyFrame;
 
 import javax.swing.*;
-
+import java.awt.*;
+import java.util.Objects;
+import javax.swing.border.Border;
 
 public class GUIRunner {
     public static void main(String[] args) {
+        ImageIcon image = new ImageIcon(Objects.requireNonNull(GUIRunner.class.getResource("/images/cipher.png")));
+        Border border = BorderFactory.createLineBorder(Color.GRAY, 3);
 
-        JFrame frame = new JFrame();
-        frame.setTitle("Caesar Cipher - JavaRush - Alexander Chasovskoy");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(900, 600);
+        // Main label with image and text
+        JLabel label = new JLabel();
+        label.setText("Welcome to Crypto World");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.BLACK);
+        label.setFont(new Font("Georgia", Font.BOLD, 26));
+        label.setIconTextGap(65);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setBounds(0, 0, 420, 470);
+
+        // Radio buttons for options
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.Y_AXIS));
+
+        JRadioButton encryptButton = new JRadioButton("Encrypt");
+        JRadioButton decryptButton = new JRadioButton("Decrypt");
+        JRadioButton bruteForceButton = new JRadioButton("Brute Force");
+
+        // Group the radio buttons so only one can be selected at a time
+        ButtonGroup group = new ButtonGroup();
+        group.add(encryptButton);
+        group.add(decryptButton);
+        group.add(bruteForceButton);
+
+        // Add radio buttons to the panel
+        radioPanel.add(encryptButton);
+        radioPanel.add(decryptButton);
+        radioPanel.add(bruteForceButton);
+
+        radioPanel.setBounds(450, 50, 200, 150); // Positioning the radio panel
+
+        // Button at the bottom-right corner
+        JButton nextButton = new JButton("Next");
+        nextButton.setBounds(730, 470, 120, 40); // Positioning the "Next" button
+
+        // Create and configure the frame
+        MyFrame frame = new MyFrame();
+        frame.setLayout(null);
+        frame.add(label);
+        frame.add(radioPanel);
+        frame.add(nextButton);
+
         frame.setVisible(true);
-//        Application application = new Application();
-//        Result result = application.run(args);
-//        System.out.println(result);
     }
 }
---
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/gui/MyFrame.java
@@ -0,0 +1,23 @@
+package ru.javarush.chasovskoy.cryptoanalyzer.gui;
+
+import ru.javarush.chasovskoy.cryptoanalyzer.GUIRunner;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        this.setTitle("Caesar Cipher - JavaRush - Alexander Chasovskoy");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(900, 600);
+
+        // Load image from resources
+        ImageIcon image = new ImageIcon(GUIRunner.class.getResource("/images/caesar-cipher.png"));
+        this.setIconImage(image.getImage());
+
+        this.setVisible(true);
+        this.getContentPane().setBackground(new Color(170, 130, 100));
+    }
+}
