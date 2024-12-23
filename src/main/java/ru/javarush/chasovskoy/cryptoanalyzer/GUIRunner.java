3df589d0c4c05978a44981d38b110c7a408aff04
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2024-12-23 23:03:09 +0100

    basic frame

--- src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/GUIRunner.java
+++ src/main/java/ru/javarush/chasovskoy/cryptoanalyzer/GUIRunner.java
@@ -2,12 +2,20 @@ package ru.javarush.chasovskoy.cryptoanalyzer;
 
 import ru.javarush.chasovskoy.cryptoanalyzer.entity.Result;
 
+import javax.swing.*;
+
 
 public class GUIRunner {
     public static void main(String[] args) {
 
-        Application application = new Application();
-        Result result = application.run(args);
-        System.out.println(result);
+        JFrame frame = new JFrame();
+        frame.setTitle("Caesar Cipher - JavaRush - Alexander Chasovskoy");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(900, 600);
+        frame.setVisible(true);
+//        Application application = new Application();
+//        Result result = application.run(args);
+//        System.out.println(result);
     }
 }
